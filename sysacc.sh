1f31b912bf735999bd84aec8320c5f995e500637
tg389 <timgoodwin1010@gmail.com>
2017-01-13 13:04:08 +0000

    changes made

--- sysacc.sh
+++ sysacc.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 pass=$(grep -m 1 'password' /etc/mysql/debian.cnf | awk '{print $3}')
-(/usr/bin/mysqld_safe &); sleep 3; echo '\n' && mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '${pass}'"
+(/usr/bin/mysqld_safe &); sleep 3; echo '\n' && \
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '${pass}'"
 service apache2 start
 
 
